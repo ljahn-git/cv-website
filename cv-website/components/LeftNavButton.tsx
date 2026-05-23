5c36c058534dc1126ec2f44884bb0bef8f1b01d6
ljahn-git <ltj05508@uga.edu>
2026-05-23 13:11:26 -0400

    Start of Basic Layout
    Main space for content created, started working on outside nav buttons

---
+++ cv-website/components/LeftNavButton.tsx
@@ -0,0 +1,5 @@
+export default function LeftNavButton() {
+    return (
+        <p className="absolute inset-0">test test</p>
+    )
+}
---
+++ cv-website/components/MainWindow.tsx
@@ -0,0 +1,8 @@
+export default function MainWindow() {
+    return(
+        <div className="flex h-screen justify-center items-center">
+            <div className="w-4/5 h-4/5 m-8 px-8 py-16 border border-8 rounded-4xl border-black">
+            </div>
+        </div>
+    )
+}
