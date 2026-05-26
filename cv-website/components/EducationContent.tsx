439655c82481c49bb70c138c5231e53aaddee029
ljahn-git <ltj05508@uga.edu>
2026-05-25 22:36:56 -0400

    Home Content Progressed
    Education content started, basic home content info is there, currently struggling with the typewriter effect.

--- cv-website/components/EducationContent.tsx
+++ cv-website/components/EducationContent.tsx
@@ -1,5 +1,18 @@
 export default function EducationContent() {
     return(
-        <p>Education test test</p>
+        // <p>Education test test</p>
+        <div className="h-full flex flex-col text-xl">
+            <div className="h-full flex">
+                <div className="h-full flex flex-col justify-center items-center font-bold">
+                    <p>University of Georgia</p>
+                    <p>2024-2026</p>
+                </div>
+                <p>Education test test</p>
+            </div>
+            <div className="h-full flex">
+                <p>Education test test</p>
+            </div>
+
+        </div>
     )
 }
--- cv-website/components/HomeContent.tsx
+++ cv-website/components/HomeContent.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import "@/css-animations.css";
 
 export default function HomeContent() {
     return(
@@ -6,22 +8,48 @@ export default function HomeContent() {
         <div className=" h-full grid grid-cols-[25%_75%] gap-4">
             <div className="min-h-full grid grid-rows-[50%_50%] gap-4">
                 <div className="flex justify-center">
-                    <Image className="h-full w-auto"
+                    <Image className="h-auto w-auto border rounded-4xl"
                             src="/images/headshot.png"
                             height={144}
                             width={144}
                             alt="Headshot of Leo Jahn"/>  
                 </div>
-                <div className="">
-                    <ul>
-                        <li>https://github.com/ltj05508</li>
-                        <li>asdf</li>
-                        <li>asdf</li>
-                        <li>asdf</li>
+                <div className="flex justify-center items-center underline text-xl">
+                    <ul className="list-disc">
+                        <li><Link target="_blank" href="https://github.com/ltj05508">GitHub 1</Link></li>
+                        <li><Link target="_blank" href="https://github.com/ljahn-git">GitHub 2</Link></li>
+                        <li><Link target="_blank" href="https://www.linkedin.com/in/leo-jahn/">LinkedIn</Link></li>
+                        <li><Link target="_blank" href="https://app.joinhandshake.com/profiles/bzarhj">Handshake</Link></li>
                     </ul>
                 </div>
             </div>
-            <p>Home test test</p>
+            <div className="flex flex-col text-xl">
+                <div className="flex">
+                    <p className="inline">My name is </p> <h1 className="inline italic text-9xl">Leo Jahn</h1>
+                </div>
+                <p>and I'm a 22 year old programmer/developer based out of Athens, Georgia.</p> <br/>
+                <p>I enjoy working on a little bit of everything, from React websites (like this one!), video games in Unity, and relational and document-oriented
+                    databases. Specifically, I'd define my skills like this:
+                </p>
+                <br/>
+
+                <p>Frontend:</p>
+                <ul className="list-disc">
+                    <li className="ml-8">HTML, CSS, Javascript (React)</li>
+                </ul>
+                <p>Backend:</p>
+                <ul className="list-disc">
+                    <li className="ml-8">Java, C/C++/C#, Python</li>
+                </ul>
+                <p>Database:</p>
+                <ul className="list-disc">
+                    <li className="ml-8">SQL, MongoDB</li>
+                </ul>
+
+                <br/>
+                <p>I'm always open to opportunities, so feel free to reach out!</p>
+
+            </div>
 
         </div>
     )
